1508d76d029b70bfa5d3c772a365d073679414c2
moos2333 <xvruikai0804@126.com>
2026-06-19 01:53:50 +0800

    Update TraitStacking.java

--- src/main/java/com/npstra/tinkerbetweenlands/content/traits/TraitStacking.java
+++ src/main/java/com/npstra/tinkerbetweenlands/content/traits/TraitStacking.java
@@ -4,10 +4,13 @@ import com.google.common.collect.ImmutableList;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import slimeknights.tconstruct.library.Util;
 import slimeknights.tconstruct.library.modifiers.ModifierNBT;
 import slimeknights.tconstruct.library.traits.AbstractTrait;
 import slimeknights.tconstruct.library.utils.ModifierTagHolder;
+import slimeknights.tconstruct.library.utils.TagUtil;
+
 import java.util.List;
 
 public class TraitStacking extends AbstractTrait {
@@ -19,21 +22,37 @@ public class TraitStacking extends AbstractTrait {
         super("stacking", 0x00AA00);
     }
 
-    @Override
-    public int onToolHeal(ItemStack tool, int amount, int newAmount, EntityLivingBase entity) {
-        int halved = newAmount / 2;
-        if (amount > REPAIR_THRESHOLD) {
-            ModifierTagHolder modtag = ModifierTagHolder.getModifier(tool, getModifierIdentifier());
-            Data data = modtag.getTagData(Data.class);
+    private Data getData(ItemStack tool, boolean createIfMissing) {
+        ModifierTagHolder modtag = ModifierTagHolder.getModifier(tool, getModifierIdentifier());
+        Data data = modtag.getTagData(Data.class);
+        if (data == null && createIfMissing) {
+            if (modtag.tag == null) {
+                NBTTagCompound newTag = new NBTTagCompound();
+                newTag.setString("identifier", getModifierIdentifier());
+                NBTTagList tagList = TagUtil.getModifiersTagList(tool);
+                tagList.appendTag(newTag);
+                TagUtil.setModifiersTagList(tool, tagList);
+                modtag = ModifierTagHolder.getModifier(tool, getModifierIdentifier());
+            }
+            data = modtag.getTagData(Data.class);
             if (data == null) {
                 data = new Data();
                 data.stack = 0;
                 data.write(modtag.tag);
                 modtag.save();
             }
-            if (data.stack < MAX_STACK) {
+        }
+        return data;
+    }
+
+    @Override
+    public int onToolHeal(ItemStack tool, int amount, int newAmount, EntityLivingBase entity) {
+        int halved = newAmount / 2;
+        if (amount > REPAIR_THRESHOLD) {
+            Data data = getData(tool, true);
+            if (data != null && data.stack < MAX_STACK) {
                 data.stack++;
-                modtag.save();
+                ModifierTagHolder.getModifier(tool, getModifierIdentifier()).save();
             }
         }
         return halved;
@@ -44,16 +63,8 @@ public class TraitStacking extends AbstractTrait {
         if (entity == null) {
             return newDamage;
         }
-        ModifierTagHolder modtag = ModifierTagHolder.getModifier(tool, getModifierIdentifier());
-        Data data = modtag.getTagData(Data.class);
-        if (data == null) {
-            data = new Data();
-            data.stack = 0;
-            data.write(modtag.tag);
-            modtag.save();
-            return newDamage;
-        }
-        if (data.stack > 0 && entity.world.rand.nextFloat() < data.stack * 0.01f) {
+        Data data = getData(tool, true);
+        if (data != null && data.stack > 0 && entity.world.rand.nextFloat() < data.stack * 0.01f) {
             return 0;
         }
         return newDamage;
@@ -61,14 +72,13 @@ public class TraitStacking extends AbstractTrait {
 
     @Override
     public List<String> getExtraInfo(ItemStack tool, NBTTagCompound modifierTag) {
-        ModifierTagHolder modtag = ModifierTagHolder.getModifier(tool, getModifierIdentifier());
-        Data data = modtag.getTagData(Data.class);
+        Data data = getData(tool, false);
         int stackCount = (data == null) ? 0 : data.stack;
         String loc = String.format(LOC_Extra, getModifierIdentifier());
         return ImmutableList.of(Util.translateFormatted(loc, stackCount));
     }
 
-    private static class Data extends ModifierNBT {
+    public static class Data extends ModifierNBT {
         public int stack;
 
         @Override
